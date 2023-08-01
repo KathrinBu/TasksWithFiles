32e88d4f0d0372d726321f2c28988a7482d49eae
Kate Bugrova <bugrovaev@mail.ru>
2023-08-02 00:04:20 +0300

    ERROR :((

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+
+import static java.util.Collections.reverse;
 
 public class Main {
     public static void main(String[] args) {
-       task1();
-
+    //    task1();
+    //    task2();
+        task3();
     }
-    public static void task1 (){
+
+    public static void task1() {
         String name1 = "a1.txt";
         String name2 = "OUTPUT.txt";
 
@@ -24,9 +28,61 @@ public class Main {
             output.close();
 
         } catch (FileNotFoundException e) {
-            System.out.println("ERROR :((");
+            System.out.println("ERROR :(");
         } catch (IOException e) {
             System.out.println("TOO much errors !");
+        }
+    }
+
+    public static void task2() {
+        String name1 = "a2.txt";
+        String name2 = "OUTPUT2.txt";
+        try {
+            Scanner sc = new Scanner(new File(name1));
+            int n = sc.nextInt();
+            int[] massive = new int[n];
+            double sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                int k = sc.nextInt();
+                massive[k]++;
+                sum = sum + massive[k];
+            }
+            double average = sum / n;
+
+            FileWriter output = new FileWriter(name2);
+            output.write("srednee Arifmeticheskoe is " + average);
+
+            output.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR :((");
+        } catch (IOException e) {
+            System.out.println("TOO much errors !!");
+        }
+    }
+
+    public static void task3() {
+        String name1 = "b1.txt";
+
+        try {
+            Scanner sc = new Scanner(new File(name1));
+            String line = sc.nextLine();
+            ArrayList<String> massive = new ArrayList<>();
+         massive.add(line);
+        List<String> words= Arrays.asList(line);
+         Collections.reverse(words);
+        System.out.println(String.join(" ", words));
+
+         // while (line != null) {
+          //   for (int i = massive.length - 1; i >= 0; i--) {
+          //        System.out.println(line);
+           //   }
+        //     System.out.println(line);
+         //     line = sc.nextLine();
+         //   }
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR :(((");
         }
     }
 }
